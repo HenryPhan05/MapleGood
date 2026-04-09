4c45297013e2e85a835dbcec5c46cf4b95f42ac4
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-04-09 15:46:39 -0600

    add router when click image

--- app/components/NavigationBarApp.tsx
+++ app/components/NavigationBarApp.tsx
@@ -1,16 +1,19 @@
+"use client"
 import Image from "next/image";
 import logoIcon from "../public/images/logo icon.png";
 import { Search, ShoppingCart, User } from "lucide-react";
-
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 export default function NavigationBarApp() {
+  const router = useRouter();
   return (
     <div style={{ backgroundColor: "#E8A800" }} className="px-5 pt-3 pb-0 font-sans flex flex-col gap-5">
 
       <div className="flex items-center gap-4">
 
-        <div className="bg-white rounded-full w-20 h-20 flex items-center justify-center flex-shrink-0 mr-5 ml-15">
+        <Link href="/user" className="bg-white rounded-full w-20 h-20 flex items-center justify-center flex-shrink-0 mr-5 ml-15">
           <Image src={logoIcon} alt="Maple Goods Logo" width={60} height={60} />
-        </div>
+        </Link>
 
 
         <div className="flex-shrink-0 leading-tight mr-20">
@@ -43,13 +46,15 @@ export default function NavigationBarApp() {
           Contact
         </button>
 
-        <div className="relative flex flex-col items-center cursor-pointer  mt-3  hover:opacity-70  hover:cursor-pointer">
+        <button
+          onClick={() => router.push("/user/cart")}
+          className="relative flex flex-col items-center cursor-pointer  mt-3  hover:opacity-70  hover:cursor-pointer ">
           <span className="absolute -top-6   text-3xl font-extrabold flex items-center justify-center">
             0
           </span>
           <ShoppingCart size={60} className="text-black" />
           <p className="absolute -right-13 mt-3 text-2xl font-bold text-black">Cart</p>
-        </div>
+        </button>
 
       </div>
 
